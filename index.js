4c3312d58f15761062ad98252ed56cb108cb5064
johnsoto <johnsotomex@gmail.com>
2022-04-24 14:44:19 -0500

    cuarto commit, no estoy seguro de los cambios

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-﻿//Variables globales
+﻿// create svg element:
+var svg = d3.select("#circle").append("svg").attr("width", 200).attr("height", 200);
+// Add the path using this helper function
+svg.append('circle')
+  .attr('cx', 100)
+  .attr('cy', 100)
+  .attr('r', 50)
+  .attr('stroke', 'black')
+  .attr('fill', '#69a3b2');
+
+//Variables globales
 let pantallaP = {
 };
 let prompt = {
